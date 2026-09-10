f7b9bbb43691f44e4125e3a6b4333b729363b54c
Bhumika Kokane <bhumikakokane@Bhumikas-MacBook-Air.local>
2026-09-10 16:39:19 +0530

    Initial GlowFit project

---
+++ script.js
@@ -0,0 +1,14 @@
+function scrollToShop() {
+
+    document.getElementById("shop").scrollIntoView({
+        behavior: "smooth"
+    });
+
+}
+
+
+function addToCart(product) {
+
+    alert(product + " Added To Cart 🛒");
+
+}
